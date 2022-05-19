829a5010b850c39c9847a261f1f0a2555f00391d
ryota-ohya-php <2022php42-student001@la-study.com>
2022-05-19 14:48:48 +0900

    booksのビュー大枠作成

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>書籍一覧</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ISBN番号</th>
+                <th></th>
+            </tr>
+        </thead>
+    </table>
+@endsection
