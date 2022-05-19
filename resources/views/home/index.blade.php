5b0fd95fa1c3b04bf480b244fafc89bd6d6ced58
abetouya01 <2022php42-student021@la-study.com>
2022-05-19 11:43:10 +0900

    layout作成

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+
+@extends('layouts.app')
+@section('content')
+    <button class="button is-primary">予約</button>
+    <button class="button is-primary">貸出</button>
+    <button class="button is-primary">返却</button>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/bulma.min.css">
+    <title>afoo</title>
+</head>
+<header>
+    <div>
+        <a href=""><img src="/imgs/abe_img.png" /></a>
+    </div>
+    
+    <a href="">会員管理</a>
+    <a href="">資料管理</a>
+    <a href="">貸出一覧</a>
+</header>
+<body>
+    @yield('content')
+</body>
+</html>
