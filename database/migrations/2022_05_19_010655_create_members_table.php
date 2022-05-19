648b37817babdd361f84cd49895b86aa9e0bbf0f
omoto-php <2022php42-student005@la-study.com>
2022-05-19 13:26:21 +0900

    マイグレーションファイルにテーブル定義を記述

---
+++ database/migrations/2022_05_19_010655_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->string('tel', 255)->nullable();
+            $table->string('email', 255)->nullable();
+            $table->date('birthday')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ database/migrations/2022_05_19_012610_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn', 13)->nullable();
+            $table->string('title', 100)->nullable();
+            $table->string('author', 100)->nullable();
+            $table->foreignId('category_id')->constrained();
+            $table->string('publisher', 50)->nullable();
+            $table->date('published_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
