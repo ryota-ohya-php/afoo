8f50416c594bd8f8d4072571f290aa1cf7a3bfdd
ryota-ohya-php <2022php42-student001@la-study.com>
2022-05-20 08:59:05 +0900

    editのビューのフレーム完成

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>資料編集画面</h1>
-    <form action="{{route('books.update',$book->id)}}" method="post">
+    <form action="{{ route('books.update',$book->id)}}" method="POST">
         @method('patch')
         @include('books.form')
         <button type="submit">更新</button>
