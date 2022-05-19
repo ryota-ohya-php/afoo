28a82d8efb08f8cc7b74e4b4aafd93fe72eafe0b
abetouya01 <2022php42-student021@la-study.com>
2022-05-19 13:55:23 +0900

    TOP画面スタイル作成

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,14 @@
-
+<!-- layouts.appのテンプレート取得-->
 @extends('layouts.app')
+
+<!-- body内の記述スタート-->
 @section('content')
-    <button class="button is-primary">予約</button>
-    <button class="button is-primary">貸出</button>
-    <button class="button is-primary">返却</button>
+
+<div class="main_content">
+    <button class="button is-primary is-large">予約</button>
+    <button class="button is-success is-large">貸出</button>
+    <button class="button is-danger is-large">返却</button>
+</div>
+
+<!-- body内の記述エンド-->
 @endsection
