f1dcce4a1f760e5c6fd99254a6abb945eb645916
abetouya01 <2022php42-student021@la-study.com>
2022-05-20 17:22:39 +0900

    会員管理完了

--- resources/views/members/form.blade.php
+++ resources/views/members/form.blade.php
@@ -17,7 +17,9 @@
     <dt>メールアドレス</dt>
     <dd><input type="email" name="email"></dd>
 </dl>
+<input type="hidden" name="confirm" value="create">
 <button type ="submit">入力確認する</button>
+
 @else
 @foreach($member as $val)
 <dl>
@@ -35,6 +37,8 @@
         <dt>メールアドレス</dt>
         <dd><input type="email" name="email" value="{{$val->email}}"></dd>
     </dl>
+    <input type="hidden" name="confirm" value="updata">
+    <input type="hidden" name="member_id" value="{{$val->id}}">
     <button type ="submit">入力確認する</button>
     @endforeach
 @endif
