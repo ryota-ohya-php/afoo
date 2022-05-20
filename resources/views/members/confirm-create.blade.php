1719544a390914e613ab4fee33ef7ca009b02a46
minagi-php2022 <2022php42-student014@la-study.com>
2022-05-20 18:08:53 +0900

    確認ビューの削除

--- resources/views/members/confirm-create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<h1>登録情報の確認</h1>
-
-<hr>
-<a href="/">戻る</a>
-@endsection
--- resources/views/members/confirm-edit.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<h1>変更の確認</h1>
-
-<hr>
-<a href="/">戻る</a>
-@endsection
