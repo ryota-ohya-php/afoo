03b37a9ebe368e3cd023b7207482b89a1c89407a
minagi-php2022 <2022php42-student014@la-study.com>
2022-05-20 10:15:49 +0900

    createビューの作成

---
+++ resources/views/members/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>会員の新規登録</h1> 
+    {{-- <form action="" method="post"> --}}
+    {{-- @csrf --}}
+        <dl>
+            <dt>会員ID</dt>
+            <dd><input type="text" name="member_id"></dd>
+            <dt>会員名</dt>
+            <dd><input type="text" name="name"></dd>
+            <dt>住所</dt>
+            <dd><input type="text" name="address"></dd>
+            <dt>電話番号</dt>
+            <dd><input type="tel" name="tel"></dd>
+            <dt>生年月日</dt>
+            <dd><input type="date" name="birthday"></dd>
+            <dt>メールアドレス</dt>
+            <dd><input type="email" name="email"></dd>
+        </dl>
+        <button type ="submit">登録する</button>
+{{-- </form> --}}
+<hr>
+<a href="/">戻る</a>
+@endsection
