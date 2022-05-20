dd1b727197c00a72de4781b343a999baec019342
minagi-php2022 <2022php42-student014@la-study.com>
2022-05-20 10:44:15 +0900

    create、editのビュー

---
+++ resources/views/members/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>会員情報の編集</h1> 
+    {{-- <form action="{{ route('members.update') }}" method="post"> --}}
+    {{-- @csrf --}}
+    {{-- @method('patch') --}}
+        <dl>
+            <dt><label>会員ID</dt>
+            <dd><input type="text" name="member_id"></label></dd>
+            <dt><label>会員名</dt>
+            <dd><input type="text" name="name"></label></dd>
+            <dt><label>住所</dt>
+            <dd><input type="text" name="address"></label></dd>
+            <dt><label>電話番号</dt>
+            <dd><input type="tel" name="tel"></label></dd>
+            <dt><label>生年月日</dt>
+            <dd><input type="date" name="birthday"></label></dd>
+            <dt><label>メールアドレス</dt>
+            <dd><input type="email" name="email"></label></dd>
+        </dl>
+        <button type ="submit">登録する</button>
+{{-- </form> --}}
+<hr>
+<a href="/">戻る</a>
+@endsection
