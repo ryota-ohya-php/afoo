bb4144bb42134fb5d3eded99c04df6fe149f4931
minagi-php2022 <2022php42-student014@la-study.com>
2022-05-23 14:13:09 +0900

    スタイル整理

--- resources/views/members/form.blade.php
+++ resources/views/members/form.blade.php
@@ -3,44 +3,53 @@
 <!-- menber/createだったら valueが空のフォームを適応 -->
 @if($_SERVER['REQUEST_URI'] == '/members/create')
 
-<dl>
-    <!--<dt>会員ID</dt>
-    <dd><input type="text" name="member_id"></dd>-->
-    <dt>会員名</dt>
-    <dd><input type="text" name="name" 
-        value=""></dd>
-    <dt>住所</dt>
-    <dd><input type="text" name="address"></dd>
-    <dt>電話番号</dt>
-    <dd><input type="tel" name="tel"></dd>
-    <dt>生年月日</dt>
-    <dd><input type="date" name="birthday"></dd>
-    <dt>メールアドレス</dt>
-    <dd><input type="email" name="email"></dd>
-</dl>
-<input type="hidden" name="confirm" value="create">
-<button type ="submit">入力確認する</button>
+<div class="main_content">
+    <div class="info_dl">
+        <dl>
+            <!--<dt>会員ID</dt>
+            <dd><input type="text" name="member_id"></dd>-->
+            <dt>会員名</dt>
+            <dd><input type="text" name="name" 
+                value=""></dd>
+            <dt>住所</dt>
+            <dd><input type="text" name="address"></dd>
+            <dt>電話番号</dt>
+            <dd><input type="tel" name="tel"></dd>
+            <dt>生年月日</dt>
+            <dd><input type="date" name="birthday"></dd>
+            <dt>メールアドレス</dt>
+            <dd><input type="email" name="email"></dd>
+        </dl>
+    </div>
+        <input type="hidden" name="confirm" value="create">
+        <button type="submit" class="mamber_button button is-warning">入力確認する</button>
+    
+</div>
 
-@else
-@foreach($member as $val)
-<dl>
-        <!--<dt>会員ID</dt>
-        <dd><input type="text" name="member_id"></dd>-->
-        <dt>会員名</dt>
-        <dd><input type="text" name="name" 
-            value="{{$val->name}}"></dd>
-        <dt>住所</dt>
-        <dd><input type="text" name="address" value="{{$val->address}}"></dd>
-        <dt>電話番号</dt>
-        <dd><input type="tel" name="tel" value="{{$val->tel}}"></dd>
-        <dt>生年月日</dt>
-        <dd><input type="date" name="birthday" value="{{$val->birthday}}"></dd>
-        <dt>メールアドレス</dt>
-        <dd><input type="email" name="email" value="{{$val->email}}"></dd>
-    </dl>
-    <input type="hidden" name="confirm" value="updata">
-    <input type="hidden" name="member_id" value="{{$val->id}}">
-    <button type ="submit">入力確認する</button>
-    @endforeach
-@endif
+    @else
+    @foreach($member as $val)
+    <div class="main_content">
+        <div class="info_dl">
+            <dl>
+                <!--<dt>会員ID</dt>
+                <dd><input type="text" name="member_id"></dd>-->
+                <dt>会員名</dt>
+                <dd><input type="text" name="name" 
+                    value="{{$val->name}}"></dd>
+                <dt>住所</dt>
+                <dd><input type="text" name="address" value="{{$val->address}}"></dd>
+                <dt>電話番号</dt>
+                <dd><input type="tel" name="tel" value="{{$val->tel}}"></dd>
+                <dt>生年月日</dt>
+                <dd><input type="date" name="birthday" value="{{$val->birthday}}"></dd>
+                <dt>メールアドレス</dt>
+                <dd><input type="email" name="email" value="{{$val->email}}"></dd>
+            </dl>
+        </div>
 
+        <input type="hidden" name="confirm" value="updata">
+        <input type="hidden" name="member_id" value="{{$val->id}}">
+        <button type="submit" class="mamber_button button is-warning">入力確認する</button>
+        @endforeach
+    @endif
+    </div>
