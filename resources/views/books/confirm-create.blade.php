4d430e0d6542a484fbfd89b0c26c6bd2e31c65e3
omoto-php <2022php42-student005@la-study.com>
2022-05-26 17:24:09 +0900

    books view 修正

--- resources/views/books/confirm-create.blade.php
+++ resources/views/books/confirm-create.blade.php
@@ -3,19 +3,14 @@
 @section('content')
 <h2 class="title txt_center">入力確認</h2>
     <div class="main_content">
-        {{-- 入力情報表示 --}}
+        <!-- 入力情報表示 -->
         @include('books.confirm-text')
-
         <form action="{{route('books.store')}}" method="post">
             @csrf
-
             @include('books.confirm-hidden')
-                {{-- 登録画面に戻るボタン --}}
-                <button type="button" class="button is-primary page_button"
-                        onclick="history.back()">登録画面に戻る</button>
-                {{-- 登録ボタン --}}
+                <!-- ボタン -->
+                <button type="button" class="button is-primary page_button"onclick="history.back()">登録画面に戻る</button>
                 <button type="submit" class="page_button button is-success">登録する</button>
         </form>
-
     </div>
 @endsection
