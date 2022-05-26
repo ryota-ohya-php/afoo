c4f207c697ada9ed491b9a50b3391e032ee3575d
omoto-php <2022php42-student005@la-study.com>
2022-05-26 15:19:43 +0900

    バリデーション修正

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -93,11 +93,12 @@ class BookController extends Controller
 
             
         ]);
-        // booksテーブルに同じisbn番号を持つ場合、登録を許可せず書籍一覧に遷移
-        if (Book::where('isbn', $request->isbn)->get()) {
+        // booksテーブルに同じisbn番号を持つデータがある場合、登録を許可せず書籍一覧に遷移
+        if (Book::where('isbn', $request->isbn)->get()->count()) {
             return redirect(route('books.index'))
             ->with('flash_message', 'すでに書籍登録されています。検索後、在庫登録をしてください。');
         } else{
+            // 登録確認画面へ遷移
             return view('books.confirm-create',['request'=>$request]);
         }
         
